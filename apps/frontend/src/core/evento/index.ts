121b553e3422340563f471f6749aef1f15a6a541
Devair Luiz Moitim <dmoitim@empro.com.br>
2024-11-20 07:37:12 -0300

    Criação de funções referentes à criação, complementação e validação do convidado

--- apps/frontend/src/core/evento/index.ts
+++ apps/frontend/src/core/evento/index.ts
@@ -1,4 +1,15 @@
 import Evento from "./model/Evento";
 import Convidado from "./model/Convidado";
+import complementarConvidado from "./functions/complementarConvidado";
+import complementarEvento from "./functions/complementarEvento";
+import criarConvidadoVazio from "./functions/criarConvidado";
+import criarEventoVazio from "./functions/criarEventoVazio";
 
-export type { Evento, Convidado };
+export type {
+    Evento,
+    Convidado,
+    complementarConvidado,
+    complementarEvento,
+    criarConvidadoVazio,
+    criarEventoVazio
+};
