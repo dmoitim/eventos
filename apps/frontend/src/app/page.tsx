0dae83248368b6233b28634a9e3cccc1e4451899
Devair Luiz Moitim <dmoitim@empro.com.br>
2024-11-20 09:02:39 -0300

    Implementação da página inicial e das rotas evento e convite

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -1,9 +1,25 @@
-import Page from "@/components/template/Page";
+import LogoGrande from "@/components/template/LogoGrande";
+import Link from "next/link";
 
 export default function Home() {
     return (
-        <Page>
-            Início
-        </Page>
+        <div className="
+            h-screen flex flex-col justify-center items-center
+            bg-[url('/background-inicio.svg')] bg-cover gap-10
+        ">
+            <div className="flex flex-col items-center gap-4">
+                <LogoGrande />
+
+                <p className="text-zinc-500 font-light w-96 leading-6 text-center select-none">
+                    Crie e gerencie o convite do seu evento de forma rápida e
+                    fácil, sem complicações.
+                </p>
+            </div>
+
+            <Link href="/evento"
+                className="botao azul text-lg uppercase">
+                Crie o seu evento
+            </Link>
+        </div>
     );
 }
